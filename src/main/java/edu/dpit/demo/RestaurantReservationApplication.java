3dcaa4ad9356f3313eff5d87f8b1eced4c95be65
Nimrod-F <foldvari_nimrod@yahoo.com>
2021-07-08 16:52:23 +0300

    Project Sturcture

---
+++ src/main/java/edu/dpit/demo/RestaurantReservationApplication.java
@@ -0,0 +1,13 @@
+package edu.dpit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/dpit/demo/RestaurantReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dpit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
